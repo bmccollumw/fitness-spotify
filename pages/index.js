1768fb34d891961b931c7fff46f7a0e35992526b
Brian McCollum <bmccollumw@gmail.com>
2023-11-16 17:02:12 -0500

    doesnt show login page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
+
 import React, { useEffect } from 'react';
 import LoginPage from '../components/LoginPage';
 import { getPlaylists } from '../utils/authUtils';
 
 const Home = () => {
-  return <LoginPage></LoginPage>
+  useEffect(() => {
+    // Check if the user is authenticated
+    const isAuthenticated = localStorage.getItem('access_token');
+
+    if (!isAuthenticated) {
+        <LoginPage></LoginPage>
+    } else {
+      // User is authenticated, fetch and display playlists
+      getPlaylists();
+    }
+  }, []);
+
+  return (
+    <div>
+      {/* Display user playlists here */}
+    </div>
+  );
 };
 
 export default Home;
